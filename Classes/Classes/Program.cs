5b70153e5926dbf87203222ea75f298a452e1a55
LacyRenee <templelacy@gmail.com>
2019-02-26 10:27:17 -0600

    initial commit of code - converting numbers to their names

---
+++ Classes/Classes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Classes
+{
+    class Program
+    {
+        public String[] ones = new string[] { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+        public String[] tens = new string[] { "ten", "eleve", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
+       
+
+    }
+}
